8dfc2b17cdd71a6c6330a63fa3359807f2235c3d
MaxLabLearn <maxlablearn@gmail.com>
2022-01-27 19:48:51 +0300

    brackets task colution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let sec = str.split('');
+  let stack = [];
+
+  function ifClose(bracket) {
+    return bracketsConfig.some(a => a[1] === bracket)
+  };
+
+  function getOpen(bracket) {
+    return bracketsConfig.find(a => a[1] === bracket)[0];
+  }
+
+  for (let i = 0 ; i < sec.length ; i++) {
+    
+    if (ifClose(sec[i])) {
+      stack[stack.length - 1] === getOpen(sec[i]) ? stack.pop() : stack.push(sec[i]);
+    } else {
+      stack.push(sec[i]);
+    }
+  }
+
+  return stack.length === 0;
 }
